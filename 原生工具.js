e23ba2a2df366601b207cb21b36089a50b091644
wiki-labplus <2477573600@qq.com>
2019-05-10 18:10:09 +0800

    Update 原生工具.js

--- 原生工具.js
+++ 原生工具.js
@@ -16,31 +16,35 @@ var minus = function(a, b) {
 // 严格相等
 1 === '1'
 // false
-1 === '1'
+1 == '1'
 // true
 
+
+/***************** 字符串/数组 操作 *******************/
+// 字符串可看作数组的一种进行操作
+
 // 字符串替换
-// '.1.2.3'.split('.').join('#')
-var replaceStr = function(replace, startStr, endStr) {
-    if (typeof(replace) == 'string') {
-        var after = replace.split(startStr).join(endStr)
+// replaceStr('.1.2.3', '.', '**') // '.1.2.3' → "**1**2**3"
+var replaceStr = function(str, before, after) {
+    if (typeof(str) == 'string') {
+        var afterReplace = str.split(before).join(after)
     } else {
         console.log('ERROR: 要替换的第一个参数不是字符串')
     }
 
-    return after
+    return afterReplace
 }
 
 // 数组
 // 末尾添加新元素
 a.push('新元素')
 
-// 字符串/数组 切片
+// 切片
 // s.slice(开始下标, 结束下标)
-s.slice(0, 3)  // 'iam'
-s.slice(1, 3)  // 'am'
+s.slice(0, 3)
+s.slice(1, 3)
 // 省略下标参数意思是取到底
-s.slice(2)   // 'mgood'
+s.slice(2)
 
 
 /***************** 测试套路 *******************/
