1bc7c0c389ffd0a3e9f5c21414bb4678a75eafba
wiki-labplus <2477573600@qq.com>
2019-05-28 17:56:33 +0800

    博客

---
+++ 博客程序/说明.js
@@ -0,0 +1,19 @@
+/*
+使用下面的功能实现博客程序
+node
+express
+ajax
+
+文件分布如下
+- appBlog.js        后端程序主文件
+- blog.js           处理 blog 数据存取的文件
+- blog.json         存储 blog 数据的文件(相当于数据库)
+- blog_index.html   博客主页的 html 文件
+- blog_detail.html  博客详情页面 html 文件
+
+所需库
+body-parser cookie-parser express multer
+
+运行
+$node appBlog.js
+*/
