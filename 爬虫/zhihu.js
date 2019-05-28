359f398e9280a13480ff3e54893e46fedfa7d684
wiki-labplus <2477573600@qq.com>
2019-05-28 17:26:03 +0800

    爬虫

--- 爬虫/zhihu.js
+++ 爬虫/zhihu.js
@@ -67,7 +67,7 @@ const answersFromBody = function(body) {
     const divs = e('.zm-item-answer')
 
     const answers = []
-    for(let i = 0; i < divs.length; i++) {
+    for (let i = 0; i < divs.length; i++) {
         let element = divs[i]
         // 获取 div 的元素并且用 movieFromDiv 解析
         // 然后加入 movies 数组中
@@ -80,7 +80,7 @@ const answersFromBody = function(body) {
 
 const writeToFile = function(path, data) {
     const fs = require('fs')
-    fs.writeFile(path, data, function(error){
+    fs.writeFile(path, data, function(error) {
         if (error != null) {
             log('--- 写入成功', path)
         } else {
@@ -94,7 +94,7 @@ const cachedUrl = function(options, callback) {
     // 先生成对应的文件
     const path = options.url.split('/').join('-').split(':').join('-')
     // 先尝试去保存的文件中读取这个 url 对应的文件
-    fs.readFile(path, function(err, data){
+    fs.readFile(path, function(err, data) {
         if (err != null) {
             // 读取这个文件失败
             // 读不到的话说明是第一次请求，那么就使用 request
@@ -117,8 +117,11 @@ const cachedUrl = function(options, callback) {
 
 const __main = function() {
     const url = 'https://www.zhihu.com/question/31515263'
-    // 替换 cookie 和 useragent 中的内容
-    // 这里 useragent 我已经替换好了, 替换上你自己的 cookie 就好了
+    /*
+     解决登陆：
+        -替换 cookie 和 useragent 中的内容
+        -这里 useragent 我已经替换好了, 替换上你自己的 cookie 就好了
+    */
     const cookie = 'd_c0="AHAAH60MvwqPTjmwvdfucHkj7wveGq79HRM=|1477399807"; _zap=1a080e65-16ac-43a9-801a-44b00d7fdaed; _ga=GA1.2.660325042.1478000638; l_cap_id="MzlmNzBjZDU3MTMzNGU5OGI3ZDFmNmEzNDI0NzFmY2Y=|1480772832|6dc134dbd59983b6a26660f65b653df563f919fb"; cap_id="ZmZiMzc2MjI4Njg5NGJiZWI2ZDk4YzA5OTZkZTg1MDY=|1480772832|60cbf6cbed5bcb72bd31acd601ba6b9cc00d2dcf"; r_cap_id="YWRkOTEyYjZlZGE0NGM2ZTllNjc4MTZlYTIzYTQ2MjY=|1480772833|e0ecc69252705fc92326f5d3a40c4a4bd43e8326"; login="MGYzZjhjOGM1NmI4NGU0MGFlYTAzOGI1MTQyY2Y1YzY=|1480772846|dd72727f01b84566c4a0a82a6dcbaaac319cc6af"; q_c1=89d3fdd12a03449dbcd7511a57e47b11|1483011670000|1477399807000; _xsrf=3aab1ba02fd3d20b00c6feb9f3e1a5c6; __utma=51854390.660325042.1478000638.1483187102.1483187102.1; __utmb=51854390.0.10.1483187102; __utmc=51854390; __utmz=51854390.1483187102.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utmv=51854390.100-1|2=registration_date=20130204=1^3=entry_date=20130204=1; z_c0=Mi4wQUFEQTJVTWFBQUFBY0FBZnJReV9DaGNBQUFCaEFsVk43bGxxV0FCUm1PQXItR01YRDF1NkprTFpfUGNCOWtQWkNn|1483187116|9ebe33e6bbe68e9ed0c4b0e361ab3f9344016384'
     const useragent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36'
     const headers = {
@@ -130,7 +133,7 @@ const __main = function() {
         url: url,
         headers: headers,
     }
-    cachedUrl(options, function(error, response, body){
+    cachedUrl(options, function(error, response, body) {
         // 回调函数的三个参数分别是  错误, 响应, 响应数据
         // 检查请求是否成功, statusCode 200 是成功的代码
         if (error === null && response.statusCode == 200) {
---
+++ 爬虫/说明.js
@@ -0,0 +1,8 @@
+/* 
+utils.js: node 环境导出函数
+douban.js: 普通的爬虫(以豆瓣电影为例)
+zhihu.js: 需要登录的爬虫(以知乎为例)
+
+动态内容的爬取(以知乎的动态内容为例)：
+要找到真正的 url → chrome-Netwrok-Preserve log-XHR(点这个就找到ajax请求的响应了)
+ */
