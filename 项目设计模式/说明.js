0516820a6d7482c2ed769002862a127b6ee1cea6
LahmYing <2477573600@qq.com>
2019-06-14 15:38:16 +0800

    设计模式-mvvm
    设计模式-mvvm

---
+++ 项目设计模式/说明.js
@@ -0,0 +1,24 @@
+/*******
+来源
+*******/
+// https://zhuanlan.zhihu.com/p/38270598?utm_source=wechat_session&utm_medium=social&utm_oi=638147540739035136
+
+
+/*******
+各模块讲解
+*******/
+// 见图：mvvm-本地
+// 这里，我们就浏览器中的Web开发来讲。
+
+// View层体现为浏览器中的DOM树，
+// View - Model则体现为近几年特别流行的虚拟DOM树，
+// Model则体现为业务逻辑和ORM（Controller被归为Model层）。
+
+
+/*******
+Virtual DOM
+*******/
+// 现在比较火的React和Vue框架了。这两个框架实质上可以理解为一种，内部实现核心是
+// 虚拟 DOM 的实现 和 虚拟 DOM 树之间的比较算法。
+
+
