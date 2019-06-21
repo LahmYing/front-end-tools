9d824b9024b132aafc5911729ec44f392299a59e
LahmYing <39322673+LahmYing@users.noreply.github.com>
2019-06-21 21:19:15 +0800

    Update index.js

--- 框架/vue/index.js
+++ 框架/vue/index.js
@@ -23,13 +23,13 @@ var app4 = new Vue({
     el: '#app-4',
     data: {
         todos: [{
-                text: '学习 JavaScript'
+                text: 'v-for'
             },
             {
-                text: '学习 Vue'
+                text: 'v-for'
             },
             {
-                text: '整个牛项目'
+                text: 'v-for'
             }
         ]
     }
@@ -65,21 +65,167 @@ Vue.component('todo-item', {
     template: '<li>{{ todo.text }}</li>'
 })
 
-var app7 = new Vue({
-    el: '#app-7',
-    data: {
-        groceryList: [{
-                id: 0,
-                text: '蔬菜'
-            },
-            {
-                id: 1,
-                text: '奶酪'
-            },
-            {
-                id: 2,
-                text: '随便其它什么人吃的东西'
-            }
-        ]
+// var app7 = new Vue({
+//     el: '#app-7',
+//     data: {
+//         groceryList: [{
+//                 id: 0,
+//                 text: '蔬菜'
+//             },
+//             {
+//                 id: 1,
+//                 text: '奶酪'
+//             },
+//             {
+//                 id: 2,
+//                 text: '随便其它什么人吃的东西'
+//             }
+//         ]
+//     }
+// })
+
+var app8 = new Vue({
+  el: '#app-8',
+  data: {
+    url: 'http://www.runoob.com',
+  }
+})
+
+var app9 = new Vue({
+  el: '#app-9',
+  data: {
+
+    // doSomething: function(){
+    //   alert('doSomething')
+    // },
+
+    doSomething2: function(){
+      alert('修饰符')
+    },
+  }
+})
+
+var app10 = new Vue({
+  el: '#app-10',
+  data: {
+
+    doSomething: function(){
+      alert('doSomething')
+    },
+
+    message: '页面加载于 ' + new Date().toLocaleString(),
+
+    // doSomething2: function(){
+    //   alert('修饰符')
+    // },
+  }
+})
+
+
+var app11 = new Vue({
+  el: '#app-11',
+  data: {
+    message: 'Hello'
+  },
+  computed: {
+    // 计算属性的 getter
+    reversedMessage: function () {
+      // `this` 指向 app11 实例
+      return this.message.split('').reverse().join('')
     }
+  }
+})
+
+
+var app12 = new Vue({
+  el: '#app-12',
+  data: {
+  },
+  // 计算属性的写法 computed
+  computed: {
+    doSomething2: function () {
+      return alert('不更新且只在渲染之初出现的计算属性：' + Date.now())
+    }
+  }
+})
+
+var app13 = new Vue({
+  el: '#app-13',
+  data: {
+    firstName: 'Foo',
+    lastName: 'Bar',
+    fullName: 'Foo Bar'
+  },
+  watch: {
+    firstName: function (val) {
+      this.fullName = val + ' ' + this.lastName
+    },
+    lastName: function (val) {
+      this.fullName = this.firstName + ' ' + val
+    }
+  }
+})
+
+
+var app14 = new Vue({
+  el: '#app-14',
+  data: {
+    firstName: 'Foo',
+    lastName: 'Bar',
+  },
+  computed: {
+    fullName: {
+      // getter
+      get: function () {
+        return this.firstName + ' ' + this.lastName
+      },
+      // setter
+      set: function (newValue) {
+        var names = newValue.split(' ')
+        this.firstName = names[0]
+        this.lastName = names[names.length - 1]
+      }
+    }
+  }
+})
+
+
+var watchExampleVM = new Vue({
+  el: '#app-15',
+  data: {
+    question: '',
+    answer: 'I cannot give you an answer until you ask a question!'
+  },
+  watch: {
+    // 如果 `question` 发生改变，这个函数就会运行
+    question: function (newQuestion, oldQuestion) {
+      this.answer = 'Waiting for you to stop typing...'
+      this.debouncedGetAnswer()
+    }
+  },
+  created: function () {
+    // `_.debounce` 是一个通过 Lodash 限制操作频率的函数。
+    // 在这个例子中，我们希望限制访问 yesno.wtf/api 的频率
+    // AJAX 请求直到用户输入完毕才会发出。想要了解更多关于
+    // `_.debounce` 函数 (及其近亲 `_.throttle`) 的知识，
+    // 请参考：https://lodash.com/docs#debounce
+    this.debouncedGetAnswer = _.debounce(this.getAnswer, 500)
+  },
+  methods: {
+    getAnswer: function () {
+      if (this.question.indexOf('?') === -1) {
+        this.answer = 'Questions usually contain a question mark. ;-)'
+        return
+      }
+      this.answer = 'Thinking...'
+      var vm = this
+      axios.get('https://yesno.wtf/api')
+        .then(function (response) {
+          vm.answer = _.capitalize(response.data.answer)
+        })
+        .catch(function (error) {
+          vm.answer = 'Error! Could not reach the API. ' + error
+        })
+    }
+  }
 })
