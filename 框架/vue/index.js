50c5d33031e63f5946926023a8957651ef300181
LahmYing <39322673+LahmYing@users.noreply.github.com>
2019-06-22 15:39:12 +0800

    Update index.js

--- 框架/vue/index.js
+++ 框架/vue/index.js
@@ -1,68 +1,68 @@
 var app = new Vue({
-    el: '#app',
-    data: {
-        message: 'Hello Vue!'
-    }
+  el: '#app',
+  data: {
+    message: 'Hello Vue!'
+  }
 })
 
 var app2 = new Vue({
-    el: '#app-2',
-    data: {
-        message: '页面加载于 ' + new Date().toLocaleString()
-    }
+  el: '#app-2',
+  data: {
+    message: '页面加载于 ' + new Date().toLocaleString()
+  }
 })
 
 var app3 = new Vue({
-    el: '#app-3',
-    data: {
-        seen: true
-    }
+  el: '#app-3',
+  data: {
+    seen: true
+  }
 })
 
 var app4 = new Vue({
-    el: '#app-4',
-    data: {
-        todos: [{
-                text: 'v-for'
-            },
-            {
-                text: 'v-for'
-            },
-            {
-                text: 'v-for'
-            }
-        ]
-    }
+  el: '#app-4',
+  data: {
+    todos: [{
+        text: 'v-for'
+      },
+      {
+        text: 'v-for'
+      },
+      {
+        text: 'v-for'
+      }
+    ]
+  }
 })
 
 var app5 = new Vue({
-    el: '#app-5',
-    data: {
-        message000: 'Hello Vue.js!'
-    },
-    methods: {
-        reverseMessage: function() {
-            this.message000 = this.message000.split('').reverse().join('')
-        }
+  el: '#app-5',
+  data: {
+    message000: 'Hello Vue.js!'
+  },
+  methods: {
+    reverseMessage: function() {
+      this.message000 = this.message000.split('').reverse().join('')
     }
+  }
 })
 
 var app6 = new Vue({
-    el: '#app-6',
-    data: {
-        message: 'Hello Vue!'
-    }
+  el: '#app-6',
+  data: {
+    message: 'Hello Vue!'
+  }
 })
 
 
 // 定义名为 todo-item 的新组件
 Vue.component('todo-item', {
-    // todo-item 组件现在接受一个
-    // "prop"，类似于一个自定义特性。
-    // 这个 prop 名为 todo。
-    props: ['todo'],
-    // 插入的 div
-    template: '<li>{{ todo.text }}</li>'
+  // todo-item 组件现在接受一个
+  // "prop"，类似于一个自定义特性。
+  // 这个 prop 名为 todo。
+  props: ['todo'],
+  // 插入的 div
+  template: '<li>{{ todo.text }}</li>'
 })
 
 // var app7 = new Vue({
@@ -99,7 +99,7 @@ var app9 = new Vue({
     //   alert('doSomething')
     // },
 
-    doSomething2: function(){
+    doSomething2: function() {
       alert('修饰符')
     },
   }
@@ -109,7 +109,7 @@ var app10 = new Vue({
   el: '#app-10',
   data: {
 
-    doSomething: function(){
+    doSomething: function() {
       alert('doSomething')
     },
 
@@ -129,7 +129,7 @@ var app11 = new Vue({
   },
   computed: {
     // 计算属性的 getter
-    reversedMessage: function () {
+    reversedMessage: function() {
       // `this` 指向 app11 实例
       return this.message.split('').reverse().join('')
     }
@@ -139,11 +139,10 @@ var app11 = new Vue({
 
 var app12 = new Vue({
   el: '#app-12',
-  data: {
-  },
+  data: {},
   // 计算属性的写法 computed
   computed: {
-    doSomething2: function () {
+    doSomething2: function() {
       return alert('不更新且只在渲染之初出现的计算属性：' + Date.now())
     }
   }
@@ -157,10 +156,10 @@ var app13 = new Vue({
     fullName: 'Foo Bar'
   },
   watch: {
-    firstName: function (val) {
+    firstName: function(val) {
       this.fullName = val + ' ' + this.lastName
     },
-    lastName: function (val) {
+    lastName: function(val) {
       this.fullName = this.firstName + ' ' + val
     }
   }
@@ -176,11 +175,11 @@ var app14 = new Vue({
   computed: {
     fullName: {
       // getter
-      get: function () {
+      get: function() {
         return this.firstName + ' ' + this.lastName
       },
       // setter
-      set: function (newValue) {
+      set: function(newValue) {
         var names = newValue.split(' ')
         this.firstName = names[0]
         this.lastName = names[names.length - 1]
@@ -198,12 +197,12 @@ var watchExampleVM = new Vue({
   },
   watch: {
     // 如果 `question` 发生改变，这个函数就会运行
-    question: function (newQuestion, oldQuestion) {
+    question: function(newQuestion, oldQuestion) {
       this.answer = 'Waiting for you to stop typing...'
       this.debouncedGetAnswer()
     }
   },
-  created: function () {
+  created: function() {
     // `_.debounce` 是一个通过 Lodash 限制操作频率的函数。
     // 在这个例子中，我们希望限制访问 yesno.wtf/api 的频率
     // AJAX 请求直到用户输入完毕才会发出。想要了解更多关于
@@ -212,7 +211,7 @@ var watchExampleVM = new Vue({
     this.debouncedGetAnswer = _.debounce(this.getAnswer, 500)
   },
   methods: {
-    getAnswer: function () {
+    getAnswer: function() {
       if (this.question.indexOf('?') === -1) {
         this.answer = 'Questions usually contain a question mark. ;-)'
         return
@@ -220,12 +219,75 @@ var watchExampleVM = new Vue({
       this.answer = 'Thinking...'
       var vm = this
       axios.get('https://yesno.wtf/api')
-        .then(function (response) {
+        .then(function(response) {
           vm.answer = _.capitalize(response.data.answer)
         })
-        .catch(function (error) {
+        .catch(function(error) {
           vm.answer = 'Error! Could not reach the API. ' + error
         })
     }
   }
+})
+
+
+var app16 = new Vue({
+  el: '#app-16',
+  data: {
+    isActive: true,
+    error: null
+  },
+  computed: {
+    classObject: function() {
+      return {
+        active: this.isActive && !this.error,
+        'text-danger': this.error && this.error.type === 'fatal'
+      }
+    }
+  }
+})
+
+
+var app17 = new Vue({
+  el: '#app-17',
+  data: {
+    isActive: true,
+    activeClass: 'active',
+    errorClass: 'error',
+  },
+})
+
+
+Vue.component('my-component', {
+  template: '<p class="foo bar">内容在组件的 template 里</p>'
+})
+var app18 = new Vue({
+  el: '#app-18',
+  data: {
+    isActive: true,
+  },
+})
+
+
+var app19 = new Vue({
+  el: '#app-19',
+  data: {
+    styleObject: {
+      color: 'red',
+      fontSize: '20px'
+    }
+  },
+})
+
+
+var app20 = new Vue({
+  el: '#app-20',
+  data: {
+    baseStyles: {
+      color: 'red',
+      fontSize: '20px',
+    },
+    overridingStyles: {
+      color: 'gray',
+    },
+  },
 })
